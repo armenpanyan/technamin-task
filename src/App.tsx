5c6c5cc086fbb5b854e638caefcc1716f220460f
armenpanyan <panyan8888@gmail.com>
2022-10-28 00:41:52 +0400

    check websoket server response data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const requestData = `{
+                        "cmd": "init",
+                        "params": {
+                            "device": "desktop",
+                            "language": "en-GB"
+                        },
+                        "rid": "4E50E253-6181-050B-8E40-7155F77EE9DA"
+                  }`;
+
+const pingRequestData = `{
+"cmd": "ping",
+"rid": "F46614F4-4AA8-3F42-FEE1-11A7325E21E4"
+}`;
+
 function App() {
+
+  useEffect(() => {
+    let socket = new WebSocket("wss://mob.blue-version.com/hub/ws-sport");
+
+    socket.onopen = function(e) {
+      socket.send(requestData);
+      setInterval(() => socket.send(pingRequestData), 10000)
+    };
+
+    socket.onmessage = function(event) {
+      console.log(JSON.parse(event.data));
+    };
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +44,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >:
           Tecgnamin Task
         </a>
       </header>
